5d9aa7510e5d80d18ddd94b3d1d7aa09a077f8dc
Naama Kaplan <nkaplany@gmail.com>
2022-01-17 12:31:14 +0100

    set up project using symfony, doctrine.

---
+++ src/Entity/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $password;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $username;
+
+    #[ORM\Column(type: 'integer')]
+    private $credit = 100; //set default value
+
+    #[ORM\Column(type: 'boolean')]
+    private $premiumMember = false; //set default value
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getCredit(): ?int
+    {
+        return $this->credit;
+    }
+
+    public function setCredit(int $credit): self
+    {
+        $this->credit = $credit;
+
+        return $this;
+    }
+
+    public function getPremiumMember(): ?bool
+    {
+        return $this->premiumMember;
+    }
+
+    public function setPremiumMember(bool $premiumMember): self
+    {
+        $this->premiumMember = $premiumMember;
+
+        return $this;
+    }
+}
