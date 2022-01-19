8447b998e655e467a9caebc5336539988e2c9721
Naama Kaplan <nkaplany@gmail.com>
2022-01-19 16:57:58 +0100

    working on controller

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -3,18 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\Persistence\ManagerRegistry;
 
+use App\Entity\Bookings;
+use App\Entity\Room;
+use App\Entity\User;
+
+
 class BookingController extends AbstractController
 {
     #[Route('/booking', name: 'booking')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
+//        $users = $doctrine->getRepository(User::class);
+        $roomId = $request->query->get('id');
+
         return $this->render('booking/index.html.twig', [
-            'controller_name' => 'BookingController',
+            'roomId' => $roomId,
         ]);
     }
 }
